8908e446d271ed1734bcdcb6c69314ed2f323fe3
corbra0922a <corbra0922a@mischools.org>
2024-10-02 11:13:59 -0400

    Update radionuker.py

--- radionuker.py
+++ radionuker.py
@@ -18,12 +18,13 @@ sleep(1000)
 print('1')
 sleep(1000)
 message='HI'
-recieve=radio.receive()
+radio_message=radio.receive()
 while True:
-    if recieve is not None:
+    if radio_message is not None:
         print('recived message!')
-        print(recieve)
-        sleep(5000)
+        sleep(3000)
+        print(radio_message)
+        sleep(2000)
     #1 through 84
     for i in range (1,84):
         #sets the radio channel to the value of i
