fe065050a64edcfefc8a973f8054c14a74b399a9
corbra0922a <corbra0922a@mischools.org>
2024-10-03 07:30:04 -0400

    Update radio_scanner.py

--- radio_scanner.py
+++ radio_scanner.py
@@ -10,10 +10,11 @@ while True:
      for scanner in range(1,84):
          radio.config(channel=scanner)
          print('nothing',('found'),('in group'),(scanner))
+         sleep(5)
          if message is not None:
              sleep(2000)
              print('recived message!')
              sleep(1000)
-             print(message)
+             print(message,('sent from channel',(scanner),))
              sleep(2000)
              
