fa23dc1b9a19cc1112664a118ab799f2e108e2e3
corbra0922a <corbra0922a@mischools.org>
2024-10-03 09:24:49 -0400

    Update radionuker.py

--- radionuker.py
+++ radionuker.py
@@ -5,18 +5,8 @@ import microbit
     #I can't go to channel 99 the most I can do is 84 if I go over 84 I get this error:
     #ValueError: value out of range for argument 'channel'
     #I think it's because the microbit only has 84 channels
-print('Initializing...')
-sleep(2400)
-print('Preparing...')
-sleep(1500)
-print('Applying finishing touches...')
-sleep(2000)
-print('3')
-sleep(1000)
-print('2')
-sleep(1000)
-print('1')
-sleep(1000)
+
+
 message='HI'
 while True:
     #1 through 84
@@ -25,6 +15,4 @@ while True:
         radio.config(channel=i)
         #sends the variable 'message'
         radio.send(message)
-        #print 'group' and 'i' and also adds the word 'nuked' at the end
-        print('channel',(i),('nuked'),('with'),('message'),(message))
         sleep(5)
