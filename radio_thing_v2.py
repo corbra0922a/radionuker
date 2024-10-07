cacf18c6d988ea1860f41e615b27a51b44a756f3
corbra0922a <corbra0922a@mischools.org>
2024-10-07 12:11:22 -0400

    Update radio_thing_v2.py

--- radio_thing_v2.py
+++ radio_thing_v2.py
@@ -1,28 +1,33 @@
 from microbit import *
 import radio
 import microbit
-#if mode=0 it does nothing
-#if mode=1 it starts the nuke
-#if mode=2 it does radio channel and group nuke
-#if mode=3 it stops everything and prints the controls
+#If mode=0 it just idles
+#If mode=1 it starts the nuke
+#If mode=2 it does radio channel and group nuke
+#If mode=3 it stops everything and prints the controls
+#If mode=4 controls
+#If mode=5 controls
+#If mode=6 it starts scanner
+#If mode=7 controls
+
+#I just relized I could have just had multiple varibles and have 1=on and 2=off 
+#Im dumb
 
 
-mode=0
-message='0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶'
+
+radio.on()
 print('button A=nuke')
 print('button B=stop everything')
 print('pin_logo=nuke channel and group')
-
+mode=0
+message='0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶0̶'
 radio_message='HI'
 
 while True:
     if mode==1:
-        print('NUKE MODE ACTIVE')
-        sleep(1000)
-        print('press button B to stop')
-        sleep(1500)
         #1 through 84
-        for i in range (1,84):
+        for i in range (0,84):
+            print('channel',(i),'nuked')
             #sets the radio channel to the value of i
             radio.config(channel=i)
             #sends the variable 'message'
@@ -35,6 +40,9 @@ while True:
         mode=3
     if pin_logo.is_touched():
         mode=2
+    if button_a.was_pressed() and button_b.was_pressed():
+        mode=7
+        break
         
     if mode==2:
             #declares a variable named 'radio_group' then it will count in a range of 0 to 99 then it restarts
@@ -54,9 +62,34 @@ while True:
                     #prints the radio group you nuked then the channel you nuked (example below this comment)
                     #group [group number] nuked channel [channel number] nuked
                     print('group',radio_group,('nuked'),('channel'),(radio_channel),('nuked'))
+    
     if mode==3:
         print('button A=nuke')
         print('button B=stop everything')
         print('pin_logo=nuke channel and group')
         mode=0
-        
+    if mode==4:
+        print('channel nuke')
+        sleep(1000)
+        print('press button B to stop')
+        sleep(1500)
+        mode=1
+    if mode==5:
+            print('radio channel and group nuke')
+            sleep(1000)
+            print('press button B to stop')
+            sleep(1000)
+            mode=2
+    if mode==6:
+        while True:
+             for scanner in range(0,84):
+                 radio.config(channel=scanner)
+                 print('nothing',('found'),('in channel'),(scanner))
+                 sleep(1)
+    if mode==7:
+            print('scanner mode')
+            sleep(1000)
+            print('press button B to stop')
+            sleep(1000)
+            mode=6
+
